8fe88c2cf938943749b0d9c5c721c8dcc26fef6a
Mo7ammadSale7 <mo7mmad57@gamil.com>
2021-07-28 18:20:59 +0200

    Change return view into endpint

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 import requests
+
 
 def github(request):
     repos_url = 'https://api.github.com/search/repositories?q=created:>2021-07-19&sort=stars&order=desc'
@@ -8,6 +9,7 @@ def github(request):
     for repo in repos_results["items"][0:3]:
         language_url = f'https://api.github.com/search/repositories?q=language:{repo["language"]}&sort=forks&order=desc'
         language_results = requests.get(language_url).json()
-        language.append({'name': repo["language"], 'count': language_results["total_count"]})
-        
-    return render(request, 'core/github.html', {'data': language})
+        language.append(
+            {'name': repo["language"], 'count': language_results["total_count"]})
+
+    return JsonResponse(language, safe=False)
